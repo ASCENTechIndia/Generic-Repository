ca6ae57cc8f5da59345ac678f7e11dc6b93a52fb
aniket-pandit-15 <aniket.pandit@ascentechindia.com>
2026-09-11 18:31:40 +0530

    Sidebar correct

--- Frontend/src/HOC/Navbar/Navbar.jsx
+++ Frontend/src/HOC/Navbar/Navbar.jsx
@@ -154,13 +154,12 @@ const Navbar = ({ title = "Generic Invent", isOpen, onClose }) => {
                   {/* Children */}
                   <div
                     className={`overflow-hidden transition-all duration-300 ${
-                      isOpenSection ? "max-h-120 opacity-100" : "max-h-0 opacity-0"
+                      isOpenSection ? "opacity-100" : "max-h-0 opacity-0"
                     }`}
                   >
                     <div className="space-y-1 pl-2">
                       {(section.children || []).map((item) => {
-                        const Icon =
-                          iconMap[item.MENUTITLE] || iconMap.Default;
+                        const Icon = iconMap[item.MENUTITLE] || iconMap.Default;
 
                         return (
                           <SidebarItem
